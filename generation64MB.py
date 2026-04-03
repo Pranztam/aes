1a07e192320590b2a3f672a86d44219c9cd86752
Pranztam <272929412+Pranztam@users.noreply.github.com>
2026-04-03 17:30:25 +0200

    cosmetic on generation.py

--- generation64MB.py
+++ generation64MB.py
@@ -1,11 +1,8 @@
 import os
 
-SIZE_MB = 32
-size = SIZE_MB * 1024 * 1024
-
+size = 32 * 1024 * 1024
 data = os.urandom(size)
-
 with open("input.bin", "wb") as f:
     f.write(data)
 
-print(f"Generated {size} bytes (multiple of 16)")
+print(f"Generated {size} bytes")
